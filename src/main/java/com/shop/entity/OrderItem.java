d375ad7ffbb136eb970b18fbe2fddcfa60b93eef
kwonhojin <erica9501@naver.com>
2023-04-07 23:23:10 +0900

    hojinShop 2023_04_07 ver

--- src/main/java/com/shop/entity/OrderItem.java
+++ src/main/java/com/shop/entity/OrderItem.java
@@ -9,7 +9,7 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @Setter
-public class OrderItem {
+public class OrderItem extends BaseEntity {
 
     @Id
     @GeneratedValue
@@ -28,8 +28,5 @@ public class OrderItem {
 
     private int count;
 
-    private LocalDateTime regTime;
-
-    private LocalDateTime updateTime;
 
 }
